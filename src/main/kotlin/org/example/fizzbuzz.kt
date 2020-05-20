3b8e10342cf4f5b2098f122963162f35dc1ed249
Liz Daly <liz7daly@gmail.com>
2020-05-20 20:27:40 +0100

    Update to return FizzBuzz

--- src/main/kotlin/org/example/fizzbuzz.kt
+++ src/main/kotlin/org/example/fizzbuzz.kt
@@ -4,6 +4,7 @@ class FizzBuzz {
   fun run(size: Int): List<String> {
     return (1..size).map { 
       when {
+        it % 15 == 0 -> "FizzBuzz"
         it % 3 == 0 ->  "Fizz"
         it % 5 == 0 -> "Buzz"
         else -> it.toString()
